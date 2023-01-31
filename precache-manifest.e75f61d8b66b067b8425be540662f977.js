d9edb807438163f79b41274dc2fc6ce0bf7ed7a7
Alok Gupta <alokbhr.in@gmail.com>
2023-02-01 03:03:56 +0530

    Updates

---
+++ precache-manifest.e75f61d8b66b067b8425be540662f977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5259f8fa15b09f351879",
+    "url": "/Textutils/static/css/main.1dbe3004.chunk.css"
+  },
+  {
+    "revision": "5259f8fa15b09f351879",
+    "url": "/Textutils/static/js/main.5259f8fa.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/Textutils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/Textutils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "41a7f296fc4295ad90e3",
+    "url": "/Textutils/static/js/runtime~main.41a7f296.js"
+  },
+  {
+    "revision": "ac08374652c866c4f8183f33894e44de",
+    "url": "/Textutils/index.html"
+  }
+];
